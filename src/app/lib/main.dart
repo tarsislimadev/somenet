b42993f2101c3c33cc5f2bf1d4ed1041f5176520
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-22 23:44:18 +0000

    Updating app files

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -6,11 +6,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NetworkListScreen(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(home: NetworkListScreen());
 }
 
 class NetworkListScreen extends StatefulWidget {
@@ -29,30 +25,22 @@ class _NetworkListScreenState extends State<NetworkListScreen> {
 
   Future<void> _fetchNetworks() async {
     PermissionStatus status = await Permission.locationWhenInUse.request();
+
     if (status.isGranted) {
       List<WifiResult> networks = await Wifi.list('');
-      setState(() {
-        _networks = networks;
-      });
+      setState(() { _networks = networks; });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Networks List'),
-      ),
+      appBar: AppBar(title: Text('Networks List')),
       body: _networks.isEmpty
           ? Center(child: CircularProgressIndicator())
           : ListView.builder(
               itemCount: _networks.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_networks[index].ssid),
-                  subtitle: Text(""), // Text(_networks[index].bssid),
-                );
-              },
+              itemBuilder: (context, index) => ListTile(title: Text(_networks[index].ssid))
             ),
     );
   }
