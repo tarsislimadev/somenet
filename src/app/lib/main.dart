0b9d8736ef35a20f2419a67d6a57c59eb5bb7b07
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-23 18:34:18 +0000

    Updating app files

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wifi/wifi.dart';
+import 'package:wifi_iot/wifi_iot.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
@@ -32,16 +33,38 @@ class _NetworkListScreenState extends State<NetworkListScreen> {
     }
   }
 
+  void _createHotspot(String _ssid) async {
+    try {
+      await Wifi.createHotspot(
+        ssid: _ssid,
+        password: "abc123",
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Hotspot created successfully')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to create hotspot: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Networks List')),
       body: _networks.isEmpty
-          ? Center(child: Text("none"))
+          ? Center(child: ElevatedButton(
+                onPressed: _fetchNetworks,
+                child: Text('Fetch networks'),
+              ))
           : ListView.builder(
               itemCount: _networks.length,
-              itemBuilder: (context, index) => ListTile(title: Text(_networks[index].ssid))
-            ),
+              itemBuilder: (context, index) => ElevatedButton(
+                onPressed: () => await _createHotspot(_networks[index].ssid),
+                child: Text('Create Hotspot' + _networks[index].ssid),
+              ),
+          )
     );
   }
 }
